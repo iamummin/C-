7cebaa724a50d5406b73ceef40bf1fd0db7435dc
iamummin <a01059605788@gmail.com>
2026-07-09 16:52:19 +0900

    Add files via upload

--- 2일차.cpp
+++ 2일차.cpp
@@ -1,14 +1,14 @@
-#include<stdio.h>
-#pragma warning(disable:4996)
+#include <stdio.h>
+//#pragma warning(disable:4996)
 int main() {
 	int a, b;
 	scanf("%d %d", &a, &b);
 	int A, B, C, D, E, F;
 	A = a + b;
 	B = a - b;
-
-
-
-
+	C = a * b;
+	D = a / b;
+	E = a % b;
+	printf("%d %d %d %d %d", A, B, C, D, E);
 	return 0;
 }
