e2778abd2dd6f881a478435c6d9083104dd6bf0a
iamummin <a01059605788@gmail.com>
2026-07-23 00:46:22 +0900

    Add files via upload

---
+++ 0723 내야 할 과제.cpp
@@ -0,0 +1,66 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//void main()
+//{
+//	string s(10, '_');
+//	cout << s << endl;
+//	cout << s.length() << endl;
+//	cout << s.empty() << endl;
+//
+//	s.clear();
+//	cout << s.empty() << endl;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//void main() {
+//	string s = "Hello, ISam!";
+//	cout << s.substr(7, 5) << endl;
+//
+//	s.append(" :)");
+//	s += "\n hi";
+//	cout << s << endl;
+//	s.insert(5, "??");
+//	cout << s << endl;
+//	printf("%s", s.c_str());
+//	}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//void main() {
+//	string s = "Hello!";
+//	cout << s[0] << endl;
+//	cout << s.at(1) << endl;
+//	cout << s.front() << endl;
+//	cout << s.back() << endl;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//void main() {
+//	string s = "example string";
+//	char search = 'e';
+//	int at = s.find(search, 0);
+//	cout << "'e' is at" << at << "" << endl;
+//	cout << s.rfind("e") << endl;
+//	if (s.find("xyz") == string::npos)
+//		cout << "¾øÀ½" << endl;
+//}
+
+#define _CRT_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+void main() {
+	char xx[100];
+	char yy[100];
+	int i, j;
+	gets(xx);
+}
